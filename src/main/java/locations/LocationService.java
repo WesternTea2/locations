0a0b3458fc5f08e5a8b4b7cee4cedc742133047e
WesternTea2 <peti123468@gmail.com>
2026-04-22 22:30:15 +0200

    Fixed pom.xml and LocationService

--- src/main/java/locations/LocationService.java
+++ src/main/java/locations/LocationService.java
@@ -4,6 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Type;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,11 +20,11 @@ public class LocationService {
     private List<Location> locations = Arrays.asList(
             new Location(1L, "Budapest", 47.497912, 19.040235),
             new Location(2L, "Catania", 41.497912, 14.040235),
-            new Location(3L ,"Taormina", 40.497912, 14.040235)
+            new Location(3L,"Taormina", 40.497912, 14.040235)
     );
 
     public List<LocationDto> getLocations() {
-        TypeToken<List<LocationDto>> typeToken = new TypeToken<List<LocationDto>>(){};
-        return modelMapper.map(locations, typeToken.getType());
+        Type targetListType = new TypeToken<List<LocationDto>>() {}.getType();
+        return modelMapper.map(locations, targetListType);
     }
 }
