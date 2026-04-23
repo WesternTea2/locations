f4cde8c28fa15ec2fcb08fbefa1f136f49e7bb7c
WesternTea2 <peti123468@gmail.com>
2026-04-23 16:26:07 +0200

    Fix and re-enable test classes for current API

--- src/test/java/locations/LocationsControllerRestTemplateIT.java
+++ src/test/java/locations/LocationsControllerRestTemplateIT.java
@@ -1,6 +1,7 @@
 package locations;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.resttestclient.TestRestTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,11 +10,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class LocationsControllerRestTemplateIT {
 
+    @Autowired
     TestRestTemplate restTemplate;
 
     @Test
     void testListLocations() {
-        LocationDto locationDto = restTemplate.postForObject("/api/locations", new CreateLocationCommand("Budapest", 12.321, 23.421), LocationDto.class);
+        LocationDto locationDto = restTemplate.postForObject(
+                "/api/locations",
+                new CreateLocationCommand("Budapest", 12.321, 23.421),
+                LocationDto.class
+        );
 
         assertEquals("Budapest", locationDto.getName());
     }
